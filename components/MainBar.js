206762b4554369a75541e783136499ef8ad02962
francis balimuttajjo <fbalimuttajjo@gmail.com>
2022-01-26 17:21:06 +0300

    main nav created

--- components/MainBar.js
+++ components/MainBar.js
@@ -1,8 +1,83 @@
-import React from 'react';
-import styles from '../styles/main.module.css'
+import React from "react";
+import { Button } from "@mui/material";
+import Suggestion from "./Suggestion";
+import AccountBalanceIcon from "@mui/icons-material/AccountBalance";
+import styles from "../styles/main.module.css";
 
+const categories = ["all", "ui", "feature", "ux", "bug"];
+const feedBack = [
+  {
+    comment: 50,
+    feedback: "window display",
+    description: "window freezes on start up",
+    category: "faeture",
+    upvotes: 9000,
+  },
+  {
+    comment: 4,
+    feedback: " display",
+    description: "display slow sdfghjkadfghsj",
+    category: "ui",
+    upvotes: 50,
+  },
+  {
+    comment: 2,
+    feedback: "display",
+    description: "window ereefefefe freezes on start up",
+    category: "ux",
+    upvotes: 100,
+  },
+];
 function MainBar() {
-  return <div>main bar</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.heading}>
+        <div className={styles.title}>
+          <AccountBalanceIcon sx={{ marginTop: "12px", marginLeft: "10px" }} />
+          <h5 className={styles.suggestion}>{feedBack.length} Suggestions</h5>
+        </div>
+        <div>
+          <div className={styles.label}>
+            <label htmlFor="selectBy">Select By: &nbsp;</label>
+            <select name="categories">
+              <option value="">choose Option</option>
+              {categories.map((optionValue) => (
+                <option value={optionValue} key={optionValue}>
+                  {optionValue}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+        <div>
+          <Button
+            variant="contained"
+            size="small"
+            sx={{
+              height: "2em",
+              textTransform: "lowercase",
+              backgroundColor: "#ba56a9",
+              textTransform: "lowercase",
+              marginTop: "1em",
+              marginLeft: "2em",
+            }}
+          >
+            + add Feedback
+          </Button>
+        </div>
+      </div>
+      {feedBack.map((item) => (
+        <Suggestion
+          key={item.feedback}
+          category={item.category}
+          description={item.description}
+          comment={item.comment}
+          feedback={item.feedback}
+          upvotes={item.upvotes}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default MainBar;
---
+++ components/Suggestion.js
@@ -0,0 +1,37 @@
+import React from "react";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import { IconButton } from "@mui/material";
+import CommentIcon from "@mui/icons-material/Comment";
+import styles from "../styles/suggestion.module.css";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+
+function Suggestion(props) {
+  return (
+    <div className={styles.container}>
+      <div style={{ flex: 1 }}>
+        <div className={styles.likes}>
+          <KeyboardArrowUpIcon sx={{ marginLeft: "-7px" }} />
+          <h6 className={styles.upvotes}>{props.upvotes}</h6>
+        </div>
+      </div>
+      <div className={styles.divFeedback}>
+        <h5 className={styles.feedBackTitle}>{props.feedback}</h5>
+        <h6 style={{ margin: "-20px", opacity: 0.4 }}>
+         {props.description}
+        </h6>
+
+        <h5 className={styles.category}>{props.category}</h5>
+      </div>
+      <div className={styles.comment}>
+        <CommentIcon sx={{ color: "#6696de", marginTop: "30px" }} />
+
+        <h6>{props.comment}</h6>
+        <IconButton>
+          <ChevronRightIcon />
+        </IconButton>
+      </div>
+    </div>
+  );
+}
+
+export default Suggestion;
