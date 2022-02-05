d951db05201c3e29049e70b77a2d1edb5af2c82f
francis balimuttajjo <fbalimuttajjo@gmail.com>
2022-02-06 00:29:12 +0300

    added nprogress for spinner

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,51 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 import React from "react";
-import { SessionProvider } from "next-auth/react"
+import Nprogress from 'nprogress'
+import Head from "next/head";
+import Router from "next/router"
+import { SessionProvider } from "next-auth/react";
+
+//disabling the spinner
+Nprogress.configure({ showSpinner: false });
+
+//
 function MyApp({ Component, pageProps }) {
-  
-  return (
-    <SessionProvider session={pageProps.session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+  const [loading, setLoading] = React.useState(false);
+//starting loader
+  Router.events.on("routeChangeStart", () => {
+    Nprogress.start()
+    setLoading(true);
+    
+  });
+  //ending loader
+  Router.events.on("routeChangeComplete", () => {
+    setLoading(false);
+    Nprogress.done()
    
+  });
+  //in case of an error
+  Router.events.on("routeChangeError", () => {
+    setLoading(false);
+  
+  });
+
+  return (
+    <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
+          integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ=="
+          crossorigin="anonymous"
+          referrerpolicy="no-referrer"
+        />
+      </Head>
+    
+      <SessionProvider session={pageProps.session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
   );
 }
 
 export default MyApp;
-
